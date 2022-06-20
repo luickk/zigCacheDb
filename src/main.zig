e1cb3017f61cd42ea2335422a6b1a6fd2a8d8051
Luick Klippel <luickklippel@Luicks-MacBook-Pro.local>
2022-06-20 13:13:10 +0200

    completing LocalCache

--- src/main.zig
+++ src/main.zig
@@ -2,43 +2,91 @@ const std = @import("std");
 const testing = std.testing;
 const print = std.debug.print;
 const ArrayList = std.ArrayList;
+const expect = std.testing.expect;
+const mem = std.mem;
 
 const test_allocator = std.testing.allocator;
 const Allocator = std.mem.Allocator;
 
 const localCacheErr = error{};
 
-pub fn keyValPair(comptime key_type: type, comptime val_type: type) type {
+pub fn KeyValPair(comptime KeyType: type, comptime ValType: type) type {
     return struct {
-        key: key_type,
-        val: val_type,
+        key: KeyType,
+        val: ValType,
     };
 }
 
-pub fn localCache(comptime key_type: type, comptime val_type: type) type {
+pub fn LocalCache(comptime EqlMixin: type, comptime KeyType: type, comptime ValType: type) type {
     return struct {
         const Self = @This();
 
-        key_val_store: ArrayList(keyValPair(key_type, val_type)),
-        // key_val_store: [*]keyValPair,
+        key_val_store: ArrayList(KeyValPair(KeyType, ValType)),
 
         fn init(a: Allocator) Self {
-            return Self{ .key_val_store = ArrayList(keyValPair(key_type, val_type)).init(a) };
+            return Self{ .key_val_store = ArrayList(KeyValPair(KeyType, ValType)).init(a) };
         }
 
         fn deinit(self: *Self) void {
             self.key_val_store.deinit();
         }
 
-        fn add_key_val(self: *Self, key: key_type, val: val_type) !void {
-            print("dsad: {} {} \n", .{ key.len, val.len });
-            try self.key_val_store.append(keyValPair(key_type, val_type){ .key = key, .val = val });
+        fn addKeyVal(self: *Self, key: KeyType, val: ValType) !void {
+            try self.key_val_store.append(KeyValPair(KeyType, ValType){ .key = key, .val = val });
+        }
+
+        fn exists(self: *Self, key: KeyType) !bool {
+            for (self.key_val_store.items) |key_val| {
+                if (EqlMixin.eql(key_val.key, key)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        fn removeByKey(self: *Self, key: KeyType) !bool {
+            for (self.key_val_store.items) |key_val, i| {
+                if (EqlMixin.eql(key_val.key, key)) {
+                    _ = self.key_val_store.orderedRemove(i);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        pub usingnamespace EqlMixin;
+    };
+}
+
+pub fn RemoteCacheInstance(comptime EqlMixin: type, comptime KeyType: type, comptime ValType: type) type {
+    return struct {
+        const Self = @This();
+        port: u32,
+        cache: LocalCache(EqlMixin, KeyType, ValType),
+
+        pub fn init(port: u32) Self {
+            return Self{ .port = port, .cache = LocalCache(EqlMixin, KeyType, ValType).init() };
+        }
+    };
+}
+
+fn EqlStr(comptime Ktype: type) type {
+    return struct {
+        pub fn eql(k1: Ktype, k2: Ktype) bool {
+            return mem.eql(u8, k1, k2);
         }
     };
 }
 
 test "localCache test" {
-    var cache = localCache([]const u8, []const u8).init(test_allocator);
+    var cache = LocalCache(EqlStr([]const u8), []const u8, []const u8).init(test_allocator);
     defer cache.deinit();
-    cache.add_key_val("dsad", "dsad") catch unreachable;
+
+    try expect(!try cache.exists("doesNotExist"));
+
+    try cache.addKeyVal("test1", "testVal");
+
+    try expect(try cache.removeByKey("test1"));
+
+    try expect(!try cache.exists("test1"));
 }
